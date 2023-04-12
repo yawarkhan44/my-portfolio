6f37e6386a6bb903d3eafb4ed20fc79268aa4c0d
Yawar <yawarkhan44@yahoo.com>
2023-04-12 22:58:24 +0200

    Edited About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,21 @@ import "./About.scss"
 
 const About = () => {
   return (
-    <div className="about">About</div>
+    <div className="about">
+      <h1>About me</h1>
+        <p>I am passionate about creating beautiful and intuitive websites.
+          I have a strong understanding of HTML, CSS, and JavaScript, and I am always eager to learn new 
+          technologies and techniques to improve my skills. 
+          <br/>
+          <br/>
+          As a front-end website developer I am responsible for 
+          creating the user interface and experience of a website. This
+          includes designing and coding the layout, navigation, and visual 
+          elements of a site. I have a good understanding of user experience and
+          design principles to create a site that is easy to use and visually appealing.
+          I can work closely with back-end developers and designers to bring a website to life.
+        </p>
+    </div>
   )
 }
 
