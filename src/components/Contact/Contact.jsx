c774b6e94e9750f48d7c1de3ca46c52d435fbca2
Yawar <yawarkhan44@yahoo.com>
2023-04-13 23:52:34 +0200

    Added Contact Section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,7 +3,33 @@ import "./Contact.scss"
 
 const Contact = () => {
   return (
-    <div className="contact">Contact</div>
+    <div
+        className="info-container"
+        id="contact"
+      >
+        <div className="info-wrapper">
+          <div
+            className="info-row"
+          >
+            <div className="column-1">
+              <div className="text-rapper">
+                {/* <p className="top-line">{topLine}</p> */}
+                <h1 className="heading">Contact Me</h1>
+                <p className="subtitle">
+                I am excited about the possibility of new career opportunities and am open to hearing about potential job openings.
+                As a job seeker, I am open to considering a diverse range of positions and industries.
+                </p>
+                {/* <ul className="onordered-list">{listItems}</ul> */}
+              </div>
+            </div>
+            <div className="column-2">
+              {/* <div className="image-wrapper">
+                <img src={img} alt={alt} className="image" />
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
   )
 }
 
